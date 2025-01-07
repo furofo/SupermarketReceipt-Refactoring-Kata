4e2f1b1d39bfb453353e6cec0811266fc81f9432
furofo <chriscellphonerepair@gmail.com>
2025-01-07 20:44:42 +0200

    refactored tests made simpler

--- python/tests/test_supermarket.py
+++ python/tests/test_supermarket.py
@@ -51,12 +51,7 @@ def test_ten_percent_discount_simplified():
 class TestFiveForAmountDiscount:
     # what if goes over
     def test_less_than_five_quantity(self):
-        catalog = FakeCatalog()
         toothbrush = Product("toothbrush", ProductUnit.EACH)
-        catalog.add_product(toothbrush, 0.80)
-        cart = ShoppingCart()
-        cart.add_item_quantity(toothbrush, 1)
-
         # Define the parameters for the discount function
         product = toothbrush
         quantity = 1
@@ -68,17 +63,10 @@ class TestFiveForAmountDiscount:
         # Assert that ValueError is raised
         with pytest.raises(ValueError) as excinfo:
             five_for_amount_discount(product, quantity, unit_price, offer, number_of_x, quantity_as_int)
-
-        # Optionally, check the error message
-        assert str(excinfo.value) == f"Error: Quantity of product {product} must be at least 5 to apply the discount."
+            assert str(excinfo.value) == f"Error: Quantity of product {product} must be at least 5 to apply the discount."
 
     def test_five_quantity(self):
-        catalog = FakeCatalog()
         toothbrush = Product("toothbrush", ProductUnit.EACH)
-        catalog.add_product(toothbrush, 0.80)
-        cart = ShoppingCart()
-        cart.add_item_quantity(toothbrush, 5)
-
         # Define the parameters for the discount function
         product = toothbrush
         quantity = 5
@@ -86,17 +74,11 @@ class TestFiveForAmountDiscount:
         offer = Offer(SpecialOfferType.FIVE_FOR_AMOUNT, toothbrush, argument=3.00)
         number_of_x = 1
         quantity_as_int = 5
-
         product = five_for_amount_discount(product, quantity, unit_price, offer, number_of_x, quantity_as_int)
         assert product.discount_amount == -1.0
 
     def test_ten_quantity(self):
-        catalog = FakeCatalog()
         toothbrush = Product("toothbrush", ProductUnit.EACH)
-        catalog.add_product(toothbrush, 0.80)
-        cart = ShoppingCart()
-        cart.add_item_quantity(toothbrush, 10)
-
         # Define the parameters for the discount function
         product = toothbrush
         quantity = 10
@@ -109,22 +91,12 @@ class TestFiveForAmountDiscount:
         assert product.discount_amount == -2.0
     
     def test_twenty_seven_quantity(self):
-        catalog = FakeCatalog()
         toothbrush = Product("toothbrush", ProductUnit.EACH)
-        catalog.add_product(toothbrush, 0.80)
-        cart = ShoppingCart()
-        cart.add_item_quantity(toothbrush, 27)
-        #without disocunt 21.6
-        #with discount 16.6
-        #21.6
-        #16.2
-        # Define the parameters for the discount function
         product = toothbrush
         quantity = 27
         unit_price = 0.80
         offer = Offer(SpecialOfferType.FIVE_FOR_AMOUNT, toothbrush, argument=3.0)
         number_of_x = 1
         quantity_as_int = 27
-
         product = five_for_amount_discount(product, quantity, unit_price, offer, number_of_x, quantity_as_int)
         assert product.discount_amount == -5
